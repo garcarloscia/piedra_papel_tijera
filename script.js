d6e79764bc8659b0196606814d010cc291e3d8b1
garcarloscia <garcarloscia@gmail.com>
2022-12-19 08:58:17 +0100

    Change css of results

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/* contadores para los resultados de la ronda*/
-
+/*selección del jugador */
         // buttons is a node list. It looks and acts much like an array.
 const buttons = document.querySelectorAll('button');
 
@@ -12,23 +11,12 @@ button.addEventListener('click', (e) => {
           });
         });
 
+/* contadores para los resultados de la ronda*/
 let cuentaEmpate=0;
 let cuentaGanasTu=0;
 let cuentaGanoYo=0;
 
-/* contador de partidas de la ronda de 5*/
-/* for (let i=1; i<=5; i++){
 
-    console.log(`Partida ${i} de 5`); */
-
-/*imput del jugador y condiciones para que sea piedra, papel o tijera*/
-   /*  let playerChoice=window.prompt("¿Piedra, papel o tijera?").toLowerCase();
-
-    if (playerChoice !=="piedra" && playerChoice !== "papel" && playerChoice !== "tijera"){
-        alert("Vuelve a jugar y esta vez escribe bien");
-        i--;
-
-    }else { */
         /*elección random del ordenador*/
 
     function juegaRonda(playerChoice){
@@ -46,30 +34,31 @@ let cuentaGanoYo=0;
 
         /*resultados de la partida*/
         const setComputerChoice=getComputerChoice()
-        console.log("Yo elijo " + setComputerChoice);
+        const mensaje1 = document.querySelector('#mensaje1');
+        const mensaje2 = document.querySelector('#mensaje2');
+
+        mensaje1.textContent=`Yo elijo ${setComputerChoice}`;
+    
         
 
         if (playerChoice === setComputerChoice){
             cuentaEmpate++; 
-
-            console.log(`Los dos hemos elegido ${setComputerChoice}. Hay empate, sigue jugando para desempatar`);
+            mensaje2.classList.add('empate');
+            mensaje2.textContent=`Los dos hemos elegido ${setComputerChoice}. Hay empate, sigue jugando para desempatar`;
         }
 
         else if ( (setComputerChoice==="papel" && playerChoice==="piedra") || (setComputerChoice==="piedra" && playerChoice==="tijera" )|| (setComputerChoice==="tijera" && playerChoice==="papel")){
             cuentaGanoYo++;
-            console.log(`${setComputerChoice.charAt(0).toUpperCase() + setComputerChoice.slice(1)} gana a ${playerChoice}.Yo gano, has perdido`);
+            mensaje2.classList.add('gano');
+            mensaje2.textContent=`${setComputerChoice.charAt(0).toUpperCase() + setComputerChoice.slice(1)} gana a ${playerChoice}.Yo gano, has perdido`;
         }
         else {
             cuentaGanasTu++;
-            console.log(`${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} gana a ${setComputerChoice}.Tu ganas. Enhorabuena!`);
+            mensaje2.classList.add('ganas');
+            mensaje2.textContent=`${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} gana a ${setComputerChoice}.Tu ganas. Enhorabuena!`;
         }
 
-   /*  } */
     /*resultado de la ronda*/
     console.log(`Empate: ${cuentaEmpate} Yo gano: ${cuentaGanoYo} Tu ganas:${cuentaGanasTu}`);
 }
-/* juegaRonda('piedra'); */
 
-
-
-/* } */
